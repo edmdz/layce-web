b2788e6a3aba4320ee4b4f00ecb197b090ed1f9b
Roel Mendoza <roelmdz@gmail.com>
2025-05-22 19:36:31 -0600

    Se actualiza la configuración de Astro para incluir la URL del sitio y se habilita la compresión de HTML. Se añaden nuevos archivos: un sitemap XML para mejorar la indexación en motores de búsqueda y un archivo robots.txt para gestionar el acceso de los bots. Además, se mejora el componente Layout con metadatos SEO y se optimiza la página de proyectos con datos estructurados JSON-LD, mejorando así la visibilidad y el rendimiento del sitio web.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://layce-construcciones.com',
   integrations: [tailwind()],
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  }
 });
